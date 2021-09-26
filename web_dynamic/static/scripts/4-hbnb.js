622b08aff2e2e555bbc3f95d9fe649814643e90e
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-26 15:51:14 -0500

    Refactor the functions to filtering the selected amenities.

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,11 +1,10 @@
 // const url = 'http://localhost:5001/api/v1/status//';
-const url = 'http://0.0.0.0:5001/api/v1/status/';
+const url = 'http://localhost:5001/api/v1/status/';
 // const urlPS = 'http://54.87.4.219:49916/api/v1/places_search/';
 // const urlPS = 'http://0.0.0.0:5001/api/v1/places_search/';
 const urlPS = 'http://localhost:5001/api/v1/places_search/';
 // const checkedAmenities = {};
 const selectAmenities = {};
-
 $(document).ready(function () {
   $('input').change(function () {
     if ($(this).is(':checked')) {
@@ -14,21 +13,21 @@ $(document).ready(function () {
       delete selectAmenities[$(this).attr('data-id')];
     }
     $('.amenities h4').text(Object.values(selectAmenities).join(', '));
+    console.log(selectAmenities);
   });
 
-  $.ajax({
-    url: urlPS,
-    type: 'POST',
-    data: '{}',
-    contentType: 'application/json',
-    dataType: 'json',
-    success: function (data) {
-      $('.places').append(data.map(place => {
-        return `<article>
+  $.get(url, function (data) {
+    const cls = 'available';
+    const apiStatus = $('div#api_status');
+    if (data.status == 'OK') { apiStatus.addClass(cls); } else { apiStatus.removeClass(cls); }
+  });
+
+  function createArticle (place) {
+    return `<article>
                   <div class="title_box">
                     <h2>${place.name}</h2>
                     <div class="price_by_night">
-                      ${place.price_by_night}
+                      $${place.price_by_night}
                     </div>
                   </div>
                   <div class="information">
@@ -49,28 +48,37 @@ $(document).ready(function () {
                     ${place.description}
                   </div>
                 </article>`;
-      }));
-    }
-  });
+  }
+  function getPlaces(url, body) {
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: JSON.stringify(body),
+      contentType: 'application/json',
+      dataType: 'json',
+      success: function (data) {
+        data.forEach((place) => {
+          $('section.places').append(createArticle(place));
+        });
+      }
+    });
+  };
 
-  $.get(url, function (data) {
-    const cls = 'available';
-    const apiStatus = $('div#api_status');
-    if (data.status == 'OK') { apiStatus.addClass(cls); } else { apiStatus.removeClass(cls); }
-  });
+  getPlaces (urlPS, {})
 
-  console.log(selectAmenities);
+  
+  //console.log(selectAmenities);
 
   // Buttom
 
-  // $('.container .filters button').click(() => {
-  //   $('section.places').html('');
-  //   const filters = {};
-  //   filters.amenities = Object.keys(checkedAmenities).filter((id) => typeof (checkedAmenities[id]) === 'string');
-  //   // filters.states = Object.keys(checkedStates).filter((id) => typeof (checkedStates[id]) === 'string');
-  //   // filters.cities= Object.keys(checkedCities).filter((id) => typeof (checkedCities[id]) === 'string');
-  //   getPlaces(urlPS, filters);
-  // });
-});
+  $('.container .filters button').click(() => {
+    $('section.places').html('');
+    const filters = {};
+    filters["amenities"] = Object.keys(selectAmenities)
+    //filters.states = Object.keys(checkedStates).filter((id) => typeof (checkedStates[id]) === 'string');
+    //filters.cities= Object.keys(checkedCities).filter((id) => typeof (checkedCities[id]) === 'string');
+    console.log(filters);
+    getPlaces(urlPS, filters);//
+  });
 
-console.log(selectAmenities);
+});
